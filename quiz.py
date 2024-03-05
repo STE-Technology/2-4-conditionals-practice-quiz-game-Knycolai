50f6ac05d6d707562565705295b412eec686f1a7
Knycolai <knycolainauxbill.joaquin23@ycdsbk12.ca>
2024-03-05 10:43:48 -0500

    WIP

--- quiz.py
+++ quiz.py
@@ -4,3 +4,18 @@ Author: Name
 Date: YYYY-MM-DD
 Description: A brief explanation of what this program does.
 """
+print ("Multiple-Choice Quiz Game")
+print ("")
+print ("1. Which of these agents role is a Duelist?")
+iso = ("(a) Iso")
+kj = ("(b) Killjoy")
+omen = ("(c) Omen")
+print (iso)
+print (kj)
+print (omen)
+a = input("> ")
+right_answer1 = a
+if right_answer1:
+    print("Correct!")
+else:
+    print("Incorrect.")
